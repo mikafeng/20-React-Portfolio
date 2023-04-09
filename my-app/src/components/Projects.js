2e2868e1d85264ddfe86192f7b9f071307a932b6
mikafeng <mikafengler@gmail.com>
2023-04-09 17:01:57 -0400

    redoing card bodies so imagse show up in projects

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -11,24 +11,45 @@ return(
             </div>
             <div>
                 {projects.map((project) => (
-                    <div className="card projectCard text-bg-dark shadow p-3 mb-5 bg-body-tertiary rounded">
+                    <div className="portfolio-items container-fluid mt-5">
                     <div className="row">
-                        <div className="col-md-4">
-                            <image src={project.image} className="portImg"/>
+                    <div className="col-11 mx-auto">
+                        <div className="row my-5">
+                        <div className="card projectCard">
+                        <div>
+                            <img src={project.image} alt="" className="img-fluid"/>
                         </div>
-                        <div className="col-md-8"> 
-                            <div className="card-body">
-                                <a href={project.link} className=" projectLink link-opacity-50-hover">
-                                <h1 className="card-title p-3">
-                                {project.title}
-                                </h1>
-                                </a>
-                                <p className="card-text p-2">{project.description}</p>
-                            </div>
+                        <a href={project.link} className=" projectLink link-opacity-50-hover">
+                        <div>
+                            <h2>{project.title}</h2>
                         </div>
-                    </div>
+                           <p className="card-text p-2">{project.description}</p>
+                        </a>
+                        </div>
+                        </div>
 
                     </div>
+                    </div>
+                    </div>
+
+                    // <div className="card projectCard text-bg-dark shadow p-3 mb-5 bg-body-tertiary rounded">
+                    // <div className="row">
+                    //     <div className="col-md-4">
+                    //         <image src={project.image} className="portImg"/>
+                    //     </div>
+                    //     <div className="col-md-8"> 
+                    //         <div className="card-body">
+                    //             <a href={project.link} className=" projectLink link-opacity-50-hover">
+                    //             <h1 className="card-title p-3">
+                    //             {project.title}
+                    //             </h1>
+                    //             </a>
+                    //             <p className="card-text p-2">{project.description}</p>
+                    //         </div>
+                    //     </div>
+                    // </div>
+
+                    // </div>
                 ))
                 }
             </div>
