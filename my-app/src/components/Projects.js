e84779499439dd4f531f510191666adf1f3c50ac
mikafeng <mikafengler@gmail.com>
2023-04-10 00:02:58 -0400

    flex container for projects

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -3,38 +3,39 @@ import {projects} from "../data";
 
 export default function Projects() {
 return(
-    <section id="projects" className="container-fluid m-3 p-3">
+    <section id="projects" className="container-fluid">
         <div>
             <div>
                 <h1 id="project-title">Projects</h1>
                 
             </div>
-        <div>
-            {projects.map((project) => (
-                <div className="project-container container-fluid ">
-                <div className="row">
-                <div className="col-11 mx-auto">
-                    <div className="row my-5">
+            <div className="project-container">
+                {projects.map((project) => (
+                    <div>
 
-                        <div className="card projectCard" style={{ width: '30rem' }} >
-                    
-                        <img src={project.image} alt="project-img" className="img-fluid card-img-top"/>
+                    {/* <div className="row row-cols-1 row-cols-md-2 g-4"> */}
+                    <div className="">
+                    <div className="col">
+
+                            <div className="card projectCard" style={{ width: '30rem' }} >
                         
-                        <div className="card-body">
-                        <a href={project.link} className=" projectLink link-opacity-50-hover">
-                        <h2 className="card-title">{project.title}</h2>
-                        <p className="card-text p-2">{project.description}</p>
-                        </a>
-                        </div>
-                        </div>
+                                <img src={project.image} alt="project-img" className="project-img img-fluid card-img-top"/>
+                                
+                                <div className="card-body">
+                                <a href={project.link} className=" projectLink link-opacity-50-hover">
+                                <h2 className="card-title">{project.title}</h2>
+                                <p className="card-text p-2">{project.description}</p>
+                                </a>
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
-                </div>
-                </div>
-                </div>
-            ))
-            }
-        </div>
+                    </div>
+
+                ))
+                }
+            </div>
         </div>
 
     </section>
