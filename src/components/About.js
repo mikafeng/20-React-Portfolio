09fcd1dfc30061cb6707738bae489b0a6f2c156e
mikafeng <mikafengler@gmail.com>
2023-07-17 13:54:24 -0400

    updating portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,13 @@ return(
         <div className="row p-3">
             <p className="col about p-4 m-3"> 
                 Hi there! I'm Mika. <br/>
-                I'm a new developer working with the MERN stack
-                and an Atlanta artist.
+
+                I'm a passionate full stack developer in Atlanta with expertise in education, art, and sustainability. 
+                As an educator and artist, I excel in team leadership, collaboration, creative direction, responsive curriculum development, and agile problem-solving.
+                I constantly integrate my diverse skill set to push the boundaries of technology and foster a more equitable and 
+                innovative world. 
+                I aim to craft exceptional web experiences and contribute to the ongoing evolution of user-centric solutions.
+
             </p>
         
             <div className="col p-3 skills">
