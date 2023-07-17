1ce37c47eb3548769eb851790c61fdb16ce63a59
mikafeng <mikafengler@gmail.com>
2023-07-17 14:17:05 -0400

    changed folder structure to have pages switch out via PortfolioContainer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer"
-
-
-
+import PortfolioContainer from './components/PortfolioContainer'
 import './App.css';
 
-function App() {
-  return (
-
-    <main className='App'>
-      <Navbar/>
-      <hr
-      style={{color: 'mustard'}}
-      />
-      <About/>
-      <Projects/>
-      <hr/>
-      <Contact/>
-      <Footer/>
-    </main>
-
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function PortfolioContainer() {
+    return (
+        <div> Hello </div>
+    )
+};
