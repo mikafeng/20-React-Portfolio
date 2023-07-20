0c33a29ea550226330201e81263140c1d375cada
mikafeng <mikafengler@gmail.com>
2023-07-20 16:30:38 -0400

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import '../styles/Footer.css'
 
 
@@ -9,22 +9,30 @@ export default function Footer() {
        <div className="main-footer">
         <div className="container">
             <div className="row">
-                <div className="col">
-                    <h4>First Column</h4>
-                    <ul className="list-unstyled">
-                        <li>A</li>
-                        <li>B</li>
-                        <li>C</li>
-                    </ul>
-                </div>
-                <div className="col">
-                    <h4>Second Column</h4>
-                    <ul className="list-unstyled">
-                        <li>A</li>
-                        <li>B</li>
-                        <li>C</li>
-                    </ul>
-                    </div>
+                <ul className="footer-space">
+
+ 
+                    <a href="https://github.com/mikafeng">
+                        <li className="footer-btn">
+                            <FaGithub />
+                        </li>
+                    </a>
+
+
+                        <a href="https://www.linkedin.com/in/mika-fengler-1628314b/">
+                            <li className="footer-btn"> 
+                                <FaLinkedin />
+                            </li>
+                        </a>
+
+
+                    <a href="https://www.instagram.com/mfeng_._/">
+                         <li className="footer-btn">
+                            <FaInstagram />
+                        </li>
+                    </a>
+
+                </ul>
             </div>
         </div>
        </div>
@@ -34,21 +42,9 @@ export default function Footer() {
         // <section className="footer m-3 p-5">
         //     <footer className="row">
         //         <ul>
-        //             <a href="https://github.com/mikafeng">
-        //                 <li>
-        //                     <FaGithub />
-        //                 </li>
-        //             </a>
-        //             <a href="https://www.linkedin.com/in/mika-fengler-1628314b/">
-        //                 <li>
-        //                     <FaLinkedin />
-        //                 </li>
-        //             </a>
-        //             <a href="https://www.instagram.com/mfeng_._/">
-        //                 <li>
-        //                     <FaInstagram />
-        //                 </li>
-        //             </a>
+
+
+
         //         </ul>
         //     </footer>
         // </section>
